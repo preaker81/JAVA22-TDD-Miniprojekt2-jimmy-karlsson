6d2a668cf703de1776604fc43794bb0c4d474749
Jimmy Karlsson <preaker18@gmail.com>
2023-11-23 16:33:45 +0100

    100% line coverage and first refactor of functionality. numIsPrime now works correctly and print methods added.

--- src/main/java/org/example/Prime.java
+++ src/main/java/org/example/Prime.java
@@ -4,47 +4,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This program counts the sum and the number of prime (int) numbers in interval 0-1000
+ * This class calculates and stores prime numbers within a given range.
  */
 public class Prime {
 
-    private List<Integer> primes;
-    private int count = 0;
-    private int sumOfPrimes = 0;
+    // Fields
+    private List<Integer> primes; // Stores the prime numbers found within the specified range.
+    private int count = 0; // Tracks the total count of prime numbers found.
 
+
+    private int sumOfPrimes = 0; // Stores the sum of all prime numbers found.
+
+    /**
+     * Constructor to initialize the Prime object.
+     *
+     * @param firstNumber The starting number of the range.
+     * @param lastNumber  The ending number of the range.
+     */
     public Prime(int firstNumber, int lastNumber) {
         primes = new ArrayList<>();
-        calculatePrimes(firstNumber, lastNumber);
+        // Check if the range is valid (0 to 1000)
+        if (firstNumber < 0 || lastNumber > 1000 || firstNumber > lastNumber) {
+            printInvalidRangeMsg();
+        } else {
+            // After calculation, primes list will contain all prime numbers found.
+            calculatePrimes(firstNumber, lastNumber);
+        }
     }
 
-    private boolean numIsPrime(int current, int depth) {
-        if (depth == 0) {
-            return true;
-        }
 
-        int divisibleBy = primes.get(depth);
-        if (current % divisibleBy == 0) {
-            return false;
-        }
+    private boolean numIsPrime(int number, int divisor) {
+        // Base cases
+        if (number <= 1) return false; // Numbers less than or equal to 1 are not prime
+        if (divisor > Math.sqrt(number)) return true; // No divisors found, hence prime
 
-        return numIsPrime(current, ++depth);
+        // If number is divisible by divisor, it's not a prime
+        if (number % divisor == 0) return false;
+
+        // Recursively check for next divisor
+        return numIsPrime(number, divisor + 1);
     }
 
     private void calculatePrimes(int current, int stop) {
-        if (current > stop) {
-            primes.add(count);
-            primes.add(sumOfPrimes);
-        } else if (numIsPrime(current, 0)) {
+
+        if (current > stop) return; // End of range
+
+        // Check if current number is prime and process accordingly
+        if (numIsPrime(current, 2)) { // Starting divisor is 2
             primes.add(current);
             ++count;
             sumOfPrimes += current;
-            calculatePrimes(++current, stop);
-        } else {
-            calculatePrimes(++current, stop);
         }
+
+        // Recursively check for the next number
+        calculatePrimes(current + 1, stop);
     }
 
-    public List<Integer> getPrimes() {
-        return primes;
+
+    // Print Methods
+    public void printCount() {
+        System.out.println("Hej, det finns " + count + " primtal mellan 0 och 1000");
     }
+
+    public void printSum() {
+        System.out.println("Och den totala summan av dessa primtal är " + sumOfPrimes);
+    }
+
+    public void printInvalidRangeMsg() {
+        System.out.println("Hoppsan, fel intervall angivet!");
+    }
+
+
+    // Getters & Setters
+    public int getCount() {
+        return count;
+    }
+
+    public int getSumOfPrimes() {
+        return sumOfPrimes;
+    }
+
 }
+
--- src/test/java/org/example/PrimeTest.java
+++ src/test/java/org/example/PrimeTest.java
@@ -1,7 +1,84 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeTest {
+    Prime prime;
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    @DisplayName("Tests the input with valid maximum range.")
+    void testValidRange() {
+        prime = new Prime(0, 1000);
+        assertTrue(168 == prime.getCount());
+    }
+
+    @Test
+    @DisplayName("Tests firstNumber boundary with negative value -1")
+    void testInvalidInputFirstNumberNegative() {
+        new Prime(-1, 1000);
+        String expectedResponse = "Hoppsan, fel intervall angivet!";
+        assertEquals(expectedResponse, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    @DisplayName("Tests lastNumber boundary with value over 1000")
+    void testInvalidInputLastNumberOutsideRange() {
+        new Prime(0, 1001);
+        String expectedResponse = "Hoppsan, fel intervall angivet!";
+        assertEquals(expectedResponse, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    @DisplayName("Tests the output from printCount Method with valid input.")
+    void testOutputPrintCount() {
+        prime = new Prime(0, 1000);
+        prime.printCount();
+        String expectedResponse = "Hej, det finns 168 primtal mellan 0 och 1000";
+        assertEquals(expectedResponse, outputStreamCaptor.toString().trim());
+    }
+
+
+    @Test
+    @DisplayName("Tests the output from printCount Method with valid input.")
+    void testOutputprintSum() {
+        prime = new Prime(0, 1000);
+        prime.printSum();
+        String expectedResponse = "Och den totala summan av dessa primtal är 76127";
+        assertEquals(expectedResponse, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    @DisplayName("Testing getCount getter functionality")
+    void testGetCount(){
+        prime = new Prime(0, 1000);
+        assertTrue(168 == prime.getCount());
+    }
+
+    @Test
+    @DisplayName("Testing getSum getter functionality")
+    void testGetSumOfPrimes(){
+        prime = new Prime(0, 1000);
+        assertTrue(76127 == prime.getSumOfPrimes());
+    }
 
 }
